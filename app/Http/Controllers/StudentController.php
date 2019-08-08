22d7b72ae140362a2e91abacf9f2a4dcc89cd3a2
hopeforfuture <mnbl87@gmail.com>
2019-08-09 00:19:32 +0530

    commit changes

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -28,36 +28,6 @@ class StudentController extends Controller
 			}
 		}
 		$students = Student::where('status', '=', '1')->orderBy('id', 'DESC')->paginate(10);
-		/*if(!empty($students))
-		{
-			$fav_sub_str = '';
-			foreach($students as $student)
-			{
-				$id = $student->id;
-				$name = $student->name;
-				$email = $student->email;
-				$contact = $student->contact;
-				$roll_no = $student->roll_no;
-				$fav_subjects = explode(",", $student->fav_subjects);
-				foreach($fav_subjects as $fs)
-				{
-					$fav_sub_str.=$subjectarr[$fs].",";
-				}
-				$fav_sub_str = ltrim($fav_sub_str, ",");
-				
-				$studentsarr[] = array(
-					'id'=>$id,
-					'name'=>$name,
-					'email'=>$email,
-					'contact'=>$contact,
-					'roll_no'=>$roll_no,
-					'fav_sub'=>$fav_sub_str,
-					'created'=>date('F j, Y H:i:s', strtotime($student->created))
-					
-				);
-				$fav_sub_str = '';
-			}
-		}*/
 		return view('student.index',['students'=>$students, 'subjects'=>$subjectarr])
             ->with('i', ($request->input('page', 1) - 1) * 10);
 	}
